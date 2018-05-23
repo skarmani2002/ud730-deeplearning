032d6ecf59314e30a3cd72cb124937be809d67e4
skarmani2002 <skamrani2002@gmail.com>
2018-05-23 11:34:37 +0500

    Softmax

--- cal_softmax.py
+++ cal_softmax.py
@@ -5,9 +5,7 @@ import numpy as np
 
 def softmax(x):
     """Compute softmax values for each sets of scores in x."""
-    e_x = np.exp(x - np.max(x))
-    return e_x / e_x.sum()
-    pass  # TODO: Compute and return softmax(x)
+    return np.exp(x)/np.sum(np.exp(x),axis=0)
 
 
 print(softmax(scores))
